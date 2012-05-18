0712bb384f2a60f563ec51013bf5da51f8c3f625
mastergreg <gregliras@gmail.com>
2012-05-18 13:22:34 +0300

    removed kernel tar

--- linux-2.6.37.4/curse_imp/curse.c
+++ linux-2.6.37.4/curse_imp/curse.c
@@ -1,5 +1,5 @@
 #include <linux/linkage.h>
-asmlinkage long sys_curse (int cmd, int curse_no) {
-	printk(KERN_INFO "Master, you gave me command %d on curse %d.\n");
-	return (cmd+curse_no);
+asmlinkage long sys_curse (int cmd, int curse_no, pid_t pid) {
+	printk(KERN_INFO "Master, you gave me command %d with curse %d on pid %d.\n");
+	return (cmd+curse_no+pid);
 }
