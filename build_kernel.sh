d34399ccad1f9dda02e71322e4e3867c443ed527
Aristoteles Panaras <spiritual.dragon.of.ra@gmail.com>
2012-05-21 20:37:05 +0300

    fixed initial action. makefile -> j4. removed mutex (thread owned).

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,5 +4,5 @@ kerneldir=linux-2.6.37.4
 make -C $kerneldir mrproper
 make -C $kerneldir mrproper ARCH=um
 make -C $kerneldir defconfig ARCH=um SUBARCH=$(uname -m)
-make -C $kerneldir ARCH=um SUBARCH=$(uname -m)  CC=gcc-4.4 -j6
+make -C $kerneldir ARCH=um SUBARCH=$(uname -m)  CC=gcc-4.4 -j4
 
