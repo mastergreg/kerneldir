b5d384e6c4d8a21d3f6f4e5644817f121b7475e9
Aristoteles Panaras <spiritual.dragon.of.ra@gmail.com>
2012-05-22 15:52:16 +0300

    added call file for greg's py atomic test

---
+++ linux-2.6.37.4/curse_imp/curse_userspace/check_atomic/curse_justcall.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include <curse/curse.h>
+
+#ifdef __i386__
+# define __NR_curse 341
+#else
+# define __NR_curse 303 
+#endif
+
+long curse (int command, int curse, pid_t target) {
+	return syscall(__NR_curse, command, curse, target);
+}
