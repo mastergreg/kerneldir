2d337c8cd12fff5ce44a7f229feca28cc0091d9d
Gregory Liras <gregliras@gmail.com>
2012-05-25 14:44:30 +0300

    no_fs gets and gives back open files

--- linux-2.6.37.4/curse_imp/no_fs_cache.c
+++ linux-2.6.37.4/curse_imp/no_fs_cache.c
@@ -1,3 +1,8 @@
+#include <linux/mm.h> /* for fadvise (i think) */
+#include <linux/fs_struct.h>
+#include <linux/fdtable.h>
+
+
 #include <curse/no_fs_cache.h>
 
 void no_fs_cache_init(void) {
@@ -8,6 +13,11 @@ void no_fs_cache_destroy(void) {
 	return;
 }
 void no_fs_cache_inject(void) {
+	/* http://linux.die.net/man/2/fadvise */
+	struct files_struct * open_files = get_files_struct(current);
+
+	//sys_fadvise64_64(,0,0,FADV_DONTNEED);
+	put_files_struct(open_files);
 	return;
 }
 
