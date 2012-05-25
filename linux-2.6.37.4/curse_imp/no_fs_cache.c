752a7855aa6df135e2f2f74bdab406490e4f0c71
Gregory Liras <gregliras@gmail.com>
2012-05-25 15:53:49 +0300

    no_fs_cache compiled

--- linux-2.6.37.4/curse_imp/no_fs_cache.c
+++ linux-2.6.37.4/curse_imp/no_fs_cache.c
@@ -14,9 +14,23 @@ void no_fs_cache_destroy(void) {
 }
 void no_fs_cache_inject(void) {
 	/* http://linux.die.net/man/2/fadvise */
-	struct files_struct * open_files = get_files_struct(current);
+	struct fdtable *fdt;
+	struct files_struct * open_files;
+	int n;
 
-	sys_fadvise64_64(1,0,0,POSIX_FADV_DONTNEED);
+	rcu_read_lock();
+
+	open_files = get_files_struct(current);
+	fdt =  files_fdtable(open_files);
+
+	for(n = 0;n <= fdt->max_fds;++n) {
+		if(fcheck(n)) {
+			sys_fadvise64_64(n,0,0,POSIX_FADV_DONTNEED);
+		}
+	}
+
+	rcu_read_unlock();
+
 	put_files_struct(open_files);
 	return;
 }
