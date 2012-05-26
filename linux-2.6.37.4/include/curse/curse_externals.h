aa638fdfe7615055e851db2a995dab664f35cbfa
Aristoteles Panaras <spiritual.dragon.of.ra@gmail.com>
2012-05-26 12:28:51 +0300

    Modified COMMAND/README documents

--- linux-2.6.37.4/include/curse/curse_externals.h
+++ linux-2.6.37.4/include/curse/curse_externals.h
@@ -1,14 +1,14 @@
 #ifndef _CURSE_FORWARD_DECLARATIONS
 #define _CURSE_FORWARD_DECLARATIONS
 
-/* curse init function */
 #ifndef CONFIG_CURSES
 #define CONFIG_CURSES
 #endif
+/* curse init function */
 void curse_init (void);
 
 /* general curse trigger */
-//inline void curse_trigger_checkpoint ();
+//inline void curse_trigger ();
 
 /* fs/read_write.c */
 inline void curse_k_wrapper (void);
