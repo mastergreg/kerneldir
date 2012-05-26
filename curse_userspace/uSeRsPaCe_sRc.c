ccc8f27976de1eae7991359cd73c7d221f459d96
Aristoteles Panaras <spiritual.dragon.of.ra@gmail.com>
2012-05-26 13:30:19 +0300

    Stashing userspace. Continuing trigger

--- curse_userspace/uSeRsPaCe_sRc.c
+++ curse_userspace/uSeRsPaCe_sRc.c
@@ -2,8 +2,23 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-int main (int argc, char **argv) {
-	
+#define out_stream(...)	fprintf(stdout, __VA_ARGS__)
+#define err_stream(...)	fprintf(stderr, __VA_ARGS__)
 
+void usage (void) {
+	out_stream("Description of use\n");
+}
+void exit_status (int _) {
+	if (!_) {
+		err_stream("sample%s\n", "haha");
+		err_stream("sample2\n");
+		
+	}
+}
+
+int main (int argc, char **argv) {
+	int ret=0;
+	
+	exit_status(ret);
 	return ret;
 }
