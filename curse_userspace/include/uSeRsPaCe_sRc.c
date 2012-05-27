57004b759956267a724ffeeb9b75c03980b2a99d
Aristoteles Panaras <spiritual.dragon.of.ra@gmail.com>
2012-05-27 10:22:35 +0300

    Userspace

--- curse_userspace/include/uSeRsPaCe_sRc.c
+++ curse_userspace/include/uSeRsPaCe_sRc.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <getopt.h>
+
+//#include <curse/curse.h>
+#define CURSE_MAX_NAME_SIZE 24		//Just until we test it properly (on a kernel that has the curse; writing externally now).
 
 const char *USAGE_STR =
 "Description of use\n\
@@ -15,21 +19,25 @@ Dunno ... \n\
 	Cast curse with name NAME on process with pid PID.\n\
 -L NAME PID, --lift=NAME,PID\n\
 	Lift curse with name NAME from process with pid PID.\n\
--iC ID PID, --cast-id=ID,PID\n\
+-c ID PID, --cast-id=ID,PID\n\
 	Deploy curse with id ID on process with pid PID.\n\
--iL ID, --lift-id=ID\n\
+-l ID, --lift-id=ID\n\
 	Lift curse with id ID from process with pid PID.\n\
--{i}a CURSE ATTRIBUTE, --{id-}attribute=CURSE,ATTRIBUTE\n\
-	Change CURSE's behavior based on ATTRIBUTE. Supported attributes are:\n\
+-a NAME ATTRIBUTE, --attribute=NAME,ATTRIBUTE\n\
+	Change behavior of curse with name NAME based on ATTRIBUTE. Supported attributes are:\n\
 		Ie/Id (enable/disable inheritance)\n\
 		[U/G/S][e/d] (enable/disable user/group/superuser permissions)\n\
--{i}c CURSE PID, --{id-}check=CURSE,PID\n\
+-b ID ATTRIBUTE, --attribute=ID,ATTRIBUTE\n\
+	Change behavior of curse with id ID based on ATTRIBUTE. Supported attributes are:\n\
+		Ie/Id (enable/disable inheritance)\n\
+		[U/G/S][e/d] (enable/disable user/group/superuser permissions)\n\
+-t CURSE PID, --check=CURSE,PID\n\
 	Check if process with pid PID is tainted by CURSE.\n\
 -S, --show-rules\n\
 	Displays the rules and exits.\n\
--{i}ar, --add-rule=BIN,CURSE\n\
+-i, --add-rule=BIN,CURSE\n\
 	Add rule for CURSE to be applied on binary file BIN.\n\
--{i}rr, --remove-rule=PATH,CURSE\n\
+-r, --remove-rule=PATH,CURSE\n\
 	Remove rule for CURSE from binary file BIN.\n\
 -V\n\
 	Enable verbose output.\n\
@@ -56,8 +64,18 @@ void exit_status (int ret) {
 int main (int argc, char **argv) {
 	int ret=0;
 	int i;
+	/*Slots for arguments-to-parse.*/
+	long pid=0;							/*Defaults to 0.*/
+	_Bool curse_id_or_name=0;			/*Defaults to name.*/
+	char curse[CURSE_MAX_NAME_SIZE];	/*Holds the identifying string (before it is atoi'ed, in case of id).*/
+	//...Rules are not here yet.
+	/*Slots for getopt arguments.*/
+	static char short_rules[] = "";
+	static struct option long_options[] = {
 
-	for (i=0; i < argc; i++) {
+	};
+
+	while () {
 		/*Do sth*/;
 	}
 
