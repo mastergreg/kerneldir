8dfed33f5cc191ab2c288b31adc3bd82b5b209a1
nalfemp <nalfemp@gmail.com>
2012-05-28 18:53:31 +0300

    added curse init to cast

--- linux-2.6.37.4/curse_imp/curse.c
+++ linux-2.6.37.4/curse_imp/curse.c
@@ -363,7 +363,9 @@ int syscurse_cast (curse_id_t curse_no, pid_t target) {
 		err=1;
 	}
 	spin_unlock_irqrestore(&((target_task->curse_data).protection), spinflags);
-		//Call init after cast.
+
+	CURSE_FIELD(new_index, functions)->fun_init();
+
 	printk(KERN_INFO "Casting curse %llu to process %d %llx\n",curse_no,target,new_mask);
 
 out: 
