a74cd27888ea7d080f44052e74b4e1b21344b343
Aristoteles Panaras <spiritual.dragon.of.ra@gmail.com>
2012-05-29 13:02:09 +0300

    Fixed random_oops

--- linux-2.6.37.4/include/curse/curse_list.h
+++ linux-2.6.37.4/include/curse/curse_list.h
@@ -26,12 +26,12 @@
 #include <curse/random_oops.h>
 
 /*[ADD] The system curse listing.*/
-struct curse_list_entry curse_f ull_list[] = {
+struct curse_list_entry curse_full_list[] = {
 	{	"stub", 0x00000000	},
 	
 	{	"no_curse", 0xBEA7CE5C	},
 	{	"no_fs_cache", 0x00000002	},
-	{	"random_oops", 0xDEFEC8ED	}
+	{	"random_oops", 0xDEFEC8ED	},
 	
 	{	"sentinel", 0xABADDE5C	}	/*Curse table sentinel. Every entry after this will be ignored.*/
 };
@@ -43,12 +43,12 @@ struct curse_list_entry curse_f ull_list[] = {
 const int max_curse_no = (((sizeof (curse_full_list))/(sizeof (struct curse_list_entry)))-1);
 
 /*[ADD] The system call function pointer array.*/
-struct curse_fun_element fun_a rray[] = {
+struct curse_fun_element fun_array[] = {
 	{	stub_init, stub_destroy, stub_inject	}, /* Maybe a stub maybe not, depends on how we handle 0 :: It is a stub handling curse system activation */
 
 	{	stub_init, stub_destroy, no_curse_inject	},
 	{	no_fs_cache_init, no_fs_cache_destroy, no_fs_cache_inject	},
-	{	random_oops_init, random_oops_destroy, random_curse_inject	},
+	{	random_oops_init, random_oops_destroy, random_oops_inject	},
 
 	{	stub_init, stub_destroy, stub_inject	} /* you have made a grave mistake (sentinel speaking) */
 };
