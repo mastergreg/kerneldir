993876f61ed66f35043ec0a8e7f18943f313d4c0
Gregory Liras <gregliras@gmail.com>
2012-05-29 16:04:31 +0300

    preprocessor include, not nice...

--- linux-2.6.37.4/include/curse/curse_list.h
+++ linux-2.6.37.4/include/curse/curse_list.h
@@ -9,9 +9,6 @@
 #ifndef _CURSE_LIST_LIB
 #define _CURSE_LIST_LIB
 
-#ifndef MAX_CURSE_NO
-#define MAX_CURSE_NO 1
-#endif
 
 #ifdef __KERNEL__
 
@@ -26,21 +23,6 @@
 #include <curse/no_fs_cache.h>
 #include <curse/random_oops.h>
 
-/*[ADD] The system curse listing.*/
-struct curse_list_entry curse_full_list[] = {
-	{	"stub", 0x00000000	},
-	
-	{	"no_curse", 0xBEA7CE5C	},
-	{	"no_fs_cache", 0x00000002	},
-	{	"random_oops", 0xDEFEC8ED	},
-	
-	{	"sentinel", 0xABADDE5C	}	/*Curse table sentinel. Every entry after this will be ignored.*/
-};
-
-
-/* External linking for number of curses. */
-const int max_curse_no = (((sizeof (curse_full_list))/(sizeof (struct curse_list_entry)))-1);
-
 /*[ADD] The system call function pointer array.*/
 struct curse_fun_element fun_array[] = {
 	{	stub_init, stub_destroy, stub_inject	}, /* Maybe a stub maybe not, depends on how we handle 0 :: It is a stub handling curse system activation */
@@ -54,7 +36,30 @@ struct curse_fun_element fun_array[] = {
 
 #endif	/* __KERNEL__ */
 
+#if defined (__KERNEL__) || defined (_LIB_CURSE_USER)
+
+#ifndef MAX_CURSE_NO
+#define MAX_CURSE_NO 1
+#endif
+
+/*[ADD] The system curse listing.*/
+struct curse_list_entry curse_full_list[] = {
+	{	"stub", 0x00000000	},
+	
+	{	"no_curse", 0xBEA7CE5C	},
+	{	"no_fs_cache", 0x00000002	},
+	{	"random_oops", 0xDEFEC8ED	},
+	
+	{	"sentinel", 0xABADDE5C	}	/*Curse table sentinel. Every entry after this will be ignored.*/
+};
+
+
 #undef MAX_CURSE_NO
 #define MAX_CURSE_NO (((sizeof curse_full_list)/(sizeof (struct curse_list_entry)))-1)
+/* External linking for number of curses. */
+const int max_curse_no = (((sizeof (curse_full_list))/(sizeof (struct curse_list_entry)))-1);
+
+#endif /* __KERNEL | _LIB_CURSE_USER */
+
 
 #endif /* _CURSE_LIST_LIB */
