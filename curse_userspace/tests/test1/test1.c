e1dde8c9af968fd7e3064724151a299334352d38
nalfemp <nalfemp@gmail.com>
2012-05-30 20:54:32 +0300

    new test with new curse

--- curse_userspace/tests/test1/test1.c
+++ curse_userspace/tests/test1/test1.c
@@ -9,8 +9,8 @@ int main (int argc, char **argv) {
 /* Scenario 1: After casting no_curse, no other curse should be cast by either parent or child */	
 	parent_pid = getpid();
 
-	printf("\nActivate : %d\n",		 curse_by_name3(ACTIVATE, "no_curse", parent_pid));
-	printf("\nCast no_curse: %d\n",  curse_by_name3(CAST, "no_curse", parent_pid));
+	printf("\nActivate : %d\n",		 curse(ACTIVATE, "no_curse", parent_pid,0,NULL ));
+	printf("\nCast no_curse: %d\n",  curse(CAST, "no_curse", parent_pid, 0, NULL));
 	
 	fpid = fork();
 	if (fpid < 0) {
@@ -18,13 +18,13 @@ int main (int argc, char **argv) {
 		exit(1);
 	} 
 	if (fpid == 0) {
-		printf("\nCast no_fs_cache: %d\n", curse_by_name3(CAST, "no_fs_cache", parent_pid));
-		printf("\nCast no_fs_cache: %d\n", curse_by_name3(CAST, "no_fs_cache", getpid()));
+		printf("\nCast no_fs_cache: %d\n", curse(CAST, "no_fs_cache", parent_pid, 0, NULL));
+		printf("\nCast no_fs_cache: %d\n", curse(CAST, "no_fs_cache", getpid(), 0 ,NULL));
 	} else {
-		printf("\nCheck activity: %d\n", curse_by_name3(CHECK_CURSE_ACTIVITY, "no_curse", parent_pid));
+		printf("\nCheck activity: %d\n", curse(CHECK_CURSE_ACTIVITY, "no_curse", parent_pid,0, NULL));
 	}
 
-	printf("\nDeactivate: %d\n", 	 curse_by_name3(DEACTIVATE, "no_curse", parent_pid));
+	printf("\nDeactivate: %d\n", 	 curse(DEACTIVATE, "no_curse", parent_pid, 0, NULL ));
 
 	return 0;
 }
