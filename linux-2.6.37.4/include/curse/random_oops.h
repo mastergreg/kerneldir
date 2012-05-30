98384244480e89514bec369da586dec1c95e4a29
indorilftw <vgerak@gmail.com>
2012-05-31 00:50:16 +0300

    Code cleanup-polishing v2

--- linux-2.6.37.4/include/curse/random_oops.h
+++ linux-2.6.37.4/include/curse/random_oops.h
@@ -1,8 +1,8 @@
 #ifndef _RANDOM_OOPS_CURSE
 #define _RANDOM_OOPS_CURSE
 
-void random_oops_init(void);
-void random_oops_destroy(void);
-void random_oops_inject(uint64_t);
+void random_oops_init (void);
+void random_oops_destroy (void);
+void random_oops_inject (uint64_t);
 
 #endif	/* _RANDOM_OOPS_CURSE */
--- linux-2.6.37.4/include/curse/stub_curse.h
+++ linux-2.6.37.4/include/curse/stub_curse.h
@@ -1,8 +1,8 @@
 #ifndef _STUB_CURSE
 #define _STUB_CURSE
 
-void stub_init(void /*or not*/);
-void stub_destroy(void /*or not*/);
-void stub_inject(uint64_t);
+void stub_init (void /*or not*/);
+void stub_destroy (void /*or not*/);
+void stub_inject (uint64_t);
 
 #endif	/* _STUB_CURSE */
