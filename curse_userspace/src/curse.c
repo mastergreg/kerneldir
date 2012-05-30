20ff5cdadefa3d165f6097c028c4599299927a8d
Gregory Liras <gregliras@gmail.com>
2012-05-30 17:55:55 +0300

    sync

--- curse_userspace/src/curse.c
+++ curse_userspace/src/curse.c
@@ -30,73 +30,73 @@ sem_t list_sema;
 
 /*Init-Fin handlers.*/
 void __attribute__((constructor)) curse_init_handle() {
-    if (!sem_init(&list_sema, 1 /*0 is for thread-shared semas*/ , 1)) {
-        //...Error.
-        perror("Sema init error");
-    }
-    //...Other initializations
+	if (sem_init(&list_sema, 1 /*0 is for thread-shared semas*/ , 1) != 0) {
+		//...Error.
+		perror("Sema init error");
+	}
+	//...Other initializations
 }
 void __attribute__((destructor)) curse_fin_handle() {
-    if (!sem_destroy(&list_sema)) {
-        //...Error.
-        perror("Sema destroy error");
-    }
-    //...Other
+	if (!sem_destroy(&list_sema)) {
+		//...Error.
+		perror("Sema destroy error");
+	}
+	//...Other
 }
 
 /*Wrapper for encapsulated access to the list. Static and NULL for the first time (protected by semaphore), if allocated and initialized, it must not need semaphore access.*/
 struct curse_list_entry *get_list (void) {
-    static struct curse_list_entry *buffered_list = NULL;
-    long maxCurseNum;
+	static struct curse_list_entry *buffered_list = NULL;
+	long maxCurseNum;
 
-    if (buffered_list == NULL) {
-        if (!sem_wait(&list_sema)) {	/*Take sema.*/
-            if (buffered_list == NULL) {
-                /*Call to get max_curse_no*/
-                maxCurseNum = syscall(__NR_curse, GET_CURSE_NO, 0, 0, 0, 0);
-                /*Allocate (MAX_CURSE_NO+1)*sizeof(struct curse_list_entry)*/
-                buffered_list = (struct curse_list_entry *)calloc((maxCurseNum + 1), sizeof(struct curse_list_entry));
-                /*Call syscall and get list.*/
-                syscall(__NR_curse, LIST_ALL, 0, 0, 0, buffered_list);
+	if (buffered_list == NULL) {
+		if (!sem_wait(&list_sema)) {	/*Take sema.*/
+			if (buffered_list == NULL) {
+				/*Call to get max_curse_no*/
+				maxCurseNum = syscall(__NR_curse, GET_CURSE_NO, 0, 0, 0, 0);
+				/*Allocate (MAX_CURSE_NO+1)*sizeof(struct curse_list_entry)*/
+				buffered_list = (struct curse_list_entry *)calloc((maxCurseNum + 1), sizeof(struct curse_list_entry));
+				/*Call syscall and get list.*/
+				syscall(__NR_curse, LIST_ALL, 0, 0, 0, buffered_list);
 			}
 			if (sem_post(&list_sema)) {	 /*Release sema.*/
-                perror("Semaphore release failed");
-            }
-        } else {
+				perror("Semaphore release failed");
+			}
+		} else {
 			perror("Semaphore acquisition failed");
-            return NULL;
+			return NULL;
 		}
-    }
-    return buffered_list;
+	}
+	return buffered_list;
 } 
 
 /*Wrapper for returning the index of a curse by searching with a name.*/
 int index_from_name(const char *id) {
-    /*Search static buffered list (if not null) for occurence. That is until MAX_CURSE_NO.*/
-    int i = 0, found = 0;
-    long maxCurseNum = syscall(__NR_curse, GET_CURSE_NO, 0, 0, 0, 0);
+	/*Search static buffered list (if not null) for occurence. That is until MAX_CURSE_NO.*/
+	int i = 0, found = 0;
+	long maxCurseNum = syscall(__NR_curse, GET_CURSE_NO, 0, 0, 0, 0);
 	const struct  curse_list_entry *list;
 
 	printf("max number is: %ld\n", maxCurseNum);
-    list = get_list();
-    if (list != NULL) {
-        for(i = 0; i < maxCurseNum; ++i) {
-			printf("List name: %s - ID: %s\n", list[i].curse_name, id);
-            if (strcmp(list[i].curse_name, id) == 0) {
-                found = 1;
-                break;
-            }
-        }
-        if (found == 1) {
-            return i;
-        } else {
-            perror("Curse not found");
-            return -1;  //not found
-        }
-    } else {
-        perror("Curse list is empty");
-        return -2;      //empty
-    }
+	list = get_list();
+	if (list != NULL) {
+		for(i = 0; i < maxCurseNum; ++i) {
+			printf("List name: %s - CID: %llu ID: %s\n", list[i].curse_name, (uint64_t) list[i].curse_id,id);
+			if (strcmp(list[i].curse_name, id) == 0) {
+				found = 1;
+				break;
+			}
+		}
+		if (found == 1) {
+			return i;
+		} else {
+			perror("Curse not found");
+			return -1;  //not found
+		}
+	} else {
+		perror("Curse list is empty");
+		return -2;      //empty
+	}
 }
 
 long curse(int command, int curse_no, pid_t target, int ctrl, char* userbuf ) {
