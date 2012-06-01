ca311de3bda93ea2b1f628950e5066c0e3ba4db6
nalfemp <nalfemp@gmail.com>
2012-06-01 16:43:07 +0300

    inherit test

---
+++ curse_userspace/tests/inheritance/inheritance.c
@@ -0,0 +1,55 @@
+#include <curse/curse.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+int main (int argc, char **argv) {
+	pid_t parent_pid, fpid, fpid2;
+
+/* Scenario 1: After casting no_curse, no other curse should be cast by either parent or child */	
+	parent_pid = getpid();
+	printf("\nActivate random_oops: %d\n",		 curse(ACTIVATE, "random_oops", parent_pid,0,NULL ));
+	printf("\nActivate no_fs_cache: %d\n",		 curse(ACTIVATE, "no_fs_cache", parent_pid,0,NULL ));
+	printf("\nCheck if self is tainted by random_oops: %d\n",		 curse(CHECK_TAINTED_PROCESS, "random_oops", parent_pid,0,NULL ));
+	printf("\nCheck if self is tainted by no_curse: %d\n",		 curse(CHECK_TAINTED_PROCESS, "no_curse", parent_pid,0,NULL ));
+	printf("\nCheck if self is tainted by no_fs_cache: %d\n",		 curse(CHECK_TAINTED_PROCESS, "no_fs_cache", parent_pid,0,NULL ));
+	// Inheritance Check
+	printf("\nCast random_oops on self: %d\n", curse(CAST, "no_curse", parent_pid,0, NULL));
+	printf("\nCast no_fs_cache on self: %d\n", curse(CAST, "no_fs_cache", parent_pid,0, NULL));
+
+	printf("\n\n\n...Forking...\n\n");
+	fpid = fork();
+	if (fpid < 0) {
+		perror("Could not fork");
+		exit(1);
+	} 
+	if (fpid == 0) {
+		printf("\n\n\n...Forking...\n\n");
+		fpid2 = fork();
+		if (fpid2 < 0) {
+			perror("Could not fork2");
+			exit(1);
+		}
+		if (fpid2 == 0) {
+			/* Second Child */
+			printf("\nChild2: Check if self is tainted by random_oops: %d\n",		 curse(CHECK_TAINTED_PROCESS, "random_oops", parent_pid,0,NULL ));
+			printf("\nChild2: Check if self is tainted by no_fs_cache: %d\n",		 curse(CHECK_TAINTED_PROCESS, "no_fs_cache", parent_pid,0,NULL ));
+			raise(SIGSTOP);	
+			exit(0);
+		} else {
+			/* First Child */
+			printf("\nChild: Check if self is tainted by random_oops: %d\n",		 curse(CHECK_TAINTED_PROCESS, "random_oops", parent_pid,0,NULL ));
+			printf("\nChild: Check if self is tainted by no_fs_cache: %d\n",		 curse(CHECK_TAINTED_PROCESS, "no_fs_cache", parent_pid,0,NULL ));
+			wait(fpid2);
+			raise(SIGSTOP);	
+			exit(0);
+		}
+	} else {
+		wait(fpid);
+		printf("\nDeactivate random_oops: %d\n",		 curse(DEACTIVATE, "random_oops", parent_pid,0,NULL ));
+		printf("\nDeactivate no_fs_cache: %d\n",		 curse(DEACTIVATE, "no_fs_cache", parent_pid,0,NULL ));
+	}
+
+	return 0;
+}
