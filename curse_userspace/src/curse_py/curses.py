f2ce071aaece107eced5a616a247be485f533617
Gregory Liras <gregliras@gmail.com>
2012-06-02 00:26:54 +0300

    fancy permission system active

--- curse_userspace/src/curse_py/curses.py
+++ curse_userspace/src/curse_py/curses.py
@@ -16,12 +16,14 @@ ILLEGAL_COMMAND = range(13)
 
 INH_ON,						\
 INH_OFF,					\
-USR_PERM_ON,				\
-USR_PERM_OFF,				\
-GRP_PERM_ON,				\
-GRP_PERM_OFF,				\
-SU_PERM_ON,					\
-SU_PERM_OFF   = range(8)
+USR_ACTIVE_PERM_ON,			\
+USR_ACTIVE_PERM_OFF,		\
+USR_PASSIVE_PERM_ON,		\
+USR_PASSIVE_PERM_OFF,		\
+SU_ACTIVE_PERM_ON,			\
+SU_ACTIVE_PERM_OFF,			\
+SU_PASSIVE_PERM_ON,			\
+SU_PASSIVE_PERM_OFF   = range(10)
 
 
 def showhelp():
@@ -170,16 +172,18 @@ def check_curse_status(switches):
 	return r
 
 def permissions(switches):
-	perms = {'u'	: USR_PERM_OFF, 
-			'U'		: USR_PERM_ON,
-			'g'		: GRP_PERM_OFF,
-			'G'		: GRP_PERM_ON,
-			's'		: SU_PERM_OFF,
-			'S'		: SU_PERM_ON}
+	perms = {'u'	: 'USR', 
+			'+'		: 'ON',
+			'-'		: 'OFF',
+			's'		: 'SU',
+			'a'		: 'ACTIVE',
+			'p'		: 'PASSIVE'}
 	try:
-		c_control = perms[switches['-p']]
+		par = switches['-p']
+		control_string = "{0}_{1}_PERM_{2}".format(perms[par[0]], perms[par[2]], perms[par[1]])
+		c_control = eval(control_string)
 		c_pid = int(switches['-P'])
-	except KeyError:
+	except KeyError, IndexError:
 		showhelp()
 		return 1
 	r = curse(CURSE_CTRL, 'stub', c_pid, c_control, None)
