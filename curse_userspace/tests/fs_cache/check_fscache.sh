cc0ed3fe852933966cedc8449c1251db8cfd71c6
nalfemp <nalfemp@gmail.com>
2012-06-04 21:47:11 +0300

    tweaked test

--- curse_userspace/tests/fs_cache/check_fscache.sh
+++ curse_userspace/tests/fs_cache/check_fscache.sh
@@ -2,25 +2,25 @@
 
 if ! [ -e /big0 ]; then
 	echo "creating big0..."
-	dd if=/dev/zero of=/big0 bs=1024k count=100
+	dd if=/dev/zero of=./big0 bs=1024k count=100
 	echo " "
 fi
 
 if ! [ -e /big1 ]; then
 	echo "creating big1..."
-	dd if=/dev/zero of=/big1 bs=1024k count=100
+	dd if=/dev/zero of=./big1 bs=1024k count=100
 	echo " "
 fi
 
-./uncache /big0
-./uncache /big1
+cursepie -a -N no_fs_cache
+cursepie -c -N no_fs_cache -P $$
 
 echo "1st read..."
-dd if=/big0 of=/dev/null bs=512k
+dd if=./big0 of=/dev/null bs=512k
 echo " "
 
 echo "2st read..."
-dd if=/big0 of=/dev/null bs=512k
+dd if=./big0 of=/dev/null bs=512k
 echo " "
 
 echo "disrupt and press enter..."
@@ -29,6 +29,7 @@ read c
 sleep 1
 
 echo "3rd read..."
-dd if=/big0 of=/dev/null bs=512k
+dd if=./big0 of=/dev/null bs=512k
 echo " "
 
+cursepie -d -N no_fs_cache
