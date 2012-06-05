221e2a3f80c19f76c5f200acd1fc03beace20cf7
Aristoteles Panaras <spiritual.dragon.of.ra@gmail.com>
2012-06-05 14:13:21 +0300

    DONT pull until it is stable. Inserted curse data element.

--- linux-2.6.37.4/include/curse/curse_types.h
+++ linux-2.6.37.4/include/curse/curse_types.h
@@ -15,21 +15,10 @@
 #ifndef _CURSE_TASK_STRUCT_DEFINED
 #define _CURSE_TASK_STRUCT_DEFINED
 
-/*Linked list data element.*//*
-struct curse_inside_data {
-	void *elem;
-	struct curse_inside_data *next;
-};*/
-
 /*Curse specific data (linked list head element).*/
-struct curse_specific_data {
 struct curse_inside_data {
 	void *elem;
 	struct curse_inside_data *next;
-} hi;
-	//...
-	void *head;
-	void *last;
 };
 
 /*Struct to-be injected in task_struct to let us keep tabs on processes.*/
@@ -43,7 +32,9 @@ struct task_curse_struct {
 	uint32_t no_fs_cache_counter;
 	uint32_t poison_counter;
 
-	struct curse_specific_data use_by_interface;
+	struct curse_specific_data {
+		struct curse_inside_data *head, *last;
+	} use_by_interface;
 };
 
 #endif	/* _CURSE_TASK_STRUCT_DEFINED */
