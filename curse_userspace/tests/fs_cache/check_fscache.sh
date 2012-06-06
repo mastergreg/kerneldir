5ff874ff752d0cded90a866adf652cb432d0663d
Gregory Liras <gregliras@gmail.com>
2012-06-07 00:45:55 +0300

    check_fs_cache script

--- curse_userspace/tests/fs_cache/check_fscache.sh
+++ curse_userspace/tests/fs_cache/check_fscache.sh
@@ -1,20 +1,44 @@
 #!/bin/sh
 
 echo "creating big0..."
-dd if=/dev/zero of=./big0 bs=1024k count=500
+dd if=/dev/zero of=./big0 bs=1024k count=200
 echo " "
 
-#cursepie -a -N no_fs_cache
-#cursepie -c -N no_fs_cache -P $$
+echo "creating big1..."
+dd if=/dev/zero of=./big1 bs=1024k count=200
+echo " "
+
 
 echo "1st read..."
 dd if=./big0 of=/dev/null bs=512k
+echo " "
+
+if [ x"$1" = x"curse" ];
+then
+	echo "Curse you!!"
+	cursepie -a -N no_fs_cache
+	cursepie -c -N no_fs_cache -P $$
+fi
+
+echo "Disruptive read..."
+dd if=./big1 of=/dev/null bs=512k
+echo " "
+
+if [ x"$1" = x"curse" ];
+then
+	echo "Heal you!!"
+	cursepie -l -N no_fs_cache -P $$
+	cursepie -d -N no_fs_cache
+fi
 
 echo "2st read..."
 dd if=./big0 of=/dev/null bs=512k
-#echo "disrupt and press enter..."
-#read c
+echo " "
+
+echo "3st read..."
+dd if=./big0 of=/dev/null bs=512k
+echo " "
+
 
 rm ./big0 
-#cursepie -l -N no_fs_cache -P $$
-#cursepie -d -N no_fs_cache
+rm ./big1
