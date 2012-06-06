2bdc97a455eda4021598cb2b56b663dc63d358c5
Aristoteles Panaras <spiritual.dragon.of.ra@gmail.com>
2012-06-06 16:31:22 +0300

    Just a little update on no_fs_cache. Nothing major.

--- linux-2.6.37.4/curse_imp/no_fs_cache.c
+++ linux-2.6.37.4/curse_imp/no_fs_cache.c
@@ -35,67 +35,41 @@ void no_fs_cache_destroy (struct task_struct *target)
 	return;
 }
 
-//uint32_t testme(uint32_t *a, uint32_t t){(*a)=t; return t;}
-//uint32_t testme2(uint32_t *A) {(*A)=0; return 0;}
-//uint32_t testme3(uint32_t *a) {(*a)++; return (*a);}
-//uint32_t testme4(uint32_t *a) {return (*a);}
-static struct fdtable *fun1 (void) {
-	struct fdtable *fdt;
-	struct files_struct *open_files;
-	open_files = get_files_struct(current);
-	fdt = files_fdtable(open_files);
-	return fdt;
-}
-static void c_loop (int lim) {
-		for (n = 0; n <= lim; ++n) {
-		 	if (fcheck(n)) {
-				sys_fadvise64_64(n, 0, 0, POSIX_FADV_DONTNEED);
-				debug("%ld's got sth up %d\n", (long)current->pid, n);
-			}
+static inline void clear_cache_loop (int lim) {
+	int n;
+	for (n = 0; n <= lim; ++n) {
+	 	if (fcheck(n)) {
+			sys_fadvise64_64(n, 0, 0, POSIX_FADV_DONTNEED);
+			debug("%ld's got sth up %d\n", (long)current->pid, n);
 		}
+	}
 }
 
 void no_fs_cache_inject (uint64_t mask)
 {
 	/* http://linux.die.net/man/2/fadvise */
 
-//	struct fdtable *fdt;
-//	struct files_struct *open_files;
-	int n;
+	struct fdtable *fdt;
+	struct files_struct *open_files;
 	uint32_t *counter;
 
 	counter = curse_get_mem(current, 0x00000002);
 	if (*counter > MAX_NO_FS_COUNT) { 
-//	if (testme4(counter) > MAX_NO_FS_COUNT) { 
 		rcu_read_lock();
-		preempt_disable();		//FIXME: Possible fix?
+//		preempt_disable();		//FIXME: Possible fix?
 
-//		open_files = get_files_struct(current);
-//		fdt = files_fdtable(open_files);
+		open_files = get_files_struct(current);
+		fdt = files_fdtable(open_files);
 
-//Apparently, the problem is in waiting inside an rcu read-side section.
-//Implementation of fadvise64_64 is at http://lxr.free-electrons.com/source/mm/fadvise.c#L86
-//also see http://lxr.free-electrons.com/source/include/linux/fs.h#L976 (f_mapping)
-//http://lxr.free-electrons.com/source/include/linux/fs.h#L646
-//and http://lxr.free-electrons.com/source/fs/file_table.c#L275 .
+		clear_cache_loop(fdt->max_fds);
 
-//		for (n = 0; n <= fdt->max_fds; ++n) {
-//		 	if (fcheck(n)) {
-//				sys_fadvise64_64(n, 0, 0, POSIX_FADV_DONTNEED);
-//				debug("%ld's got sth up %d\n", (long)current->pid, n);
-//			}
-//		}
-		c_loop(fdt->max_fds);
-
-		preempt_enable();		//FIXME: Possible fix?
+//		preempt_enable();		//FIXME: Possible fix?
 		rcu_read_unlock();
 		put_files_struct(open_files);
 		
 		*counter = 0;
-//		testme2(counter);
 	} else {
 		++(*counter);
-//		testme3(counter);
 	}
 
 	return;
